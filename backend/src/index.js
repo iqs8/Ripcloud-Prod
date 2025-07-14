4f4567ff1c0ae79cefbcf4136ff1189a4615c0b8
iqs8 <william.wisaksana@gmail.com>
2025-07-13 21:54:23 -0400

    cron import

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import { clerkMiddleware } from '@clerk/express'
 import fileUpload from "express-fileupload"
 import path from "path"
 import cors from "cors"
+import cron from "node-cron";
 
 import userRoutes from "./routes/user.route.js"
 import adminRoutes from "./routes/admin.route.js"
